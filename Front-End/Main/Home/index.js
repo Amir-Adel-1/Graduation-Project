ae8bb0c9d89dcf2ed73917101db66912f4c341c3
Amir-Adel-Sayed <amir.adel.dev@gmail.com>
2025-11-02 18:48:02 +0200

    Front-End-Part03

--- Front-End/Main/Home/index.js
+++ Front-End/Main/Home/index.js
@@ -34,3 +34,34 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
+
+
+// حركة إخفاء الناف بار أثناء التمرير
+  let lastScrollTop = 0;
+  const navbar = document.getElementById('navbar');
+
+  window.addEventListener('scroll', () => {
+    let currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+
+    // 🟢 اخفاء الناف بار عند النزول
+    if (currentScroll > lastScrollTop && currentScroll > 100) {
+      navbar.classList.add('hidden');
+    } else {
+      navbar.classList.remove('hidden');
+    }
+
+    // 🟦 تغيير لون الخلفية بعد التمرير البسيط
+    if (currentScroll > 50) {
+      navbar.classList.add('scrolled');
+    } else {
+      navbar.classList.remove('scrolled');
+    }
+
+    lastScrollTop = currentScroll <= 0 ? 0 : currentScroll;
+  });
+
+
+
+
+
+
